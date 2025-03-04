bd293d55e592c83e0479e9b4e243c8a1d4c13c29
Savlgoodman <mclqw@qq.com>
2025-03-04 09:05:26 +0800

    完善了图片上传功能的开发和全局异常类的开发

--- common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
+++ common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
@@ -3,13 +3,9 @@ package com.atguigu.lease.common.minio;
 
 import io.minio.MinioClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableConfigurationProperties(MinioProperties.class)
--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
@@ -2,11 +2,15 @@ package com.atguigu.lease.web.admin.service.impl;
 
 import com.atguigu.lease.common.minio.MinioProperties;
 import com.atguigu.lease.web.admin.service.FileService;
+import io.minio.errors.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import io.minio.*;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -22,46 +26,43 @@ public class FileServiceImpl implements FileService {
 
 
     @Override
-    public String upload(MultipartFile file) {
-        try{
-            boolean exists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioProperties.getBucketName()).build());
-            if(!exists){
-                minioClient.makeBucket(MakeBucketArgs.builder()
-                        .bucket(minioProperties.getBucketName())
-                        .build());
-                minioClient.setBucketPolicy(SetBucketPolicyArgs.builder()
-                        .bucket(minioProperties.getBucketName())
-                        .config(createBucketPolicyConfig(minioProperties.getBucketName()))
-                        .build());
-            }
-            String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
-            minioClient.putObject(PutObjectArgs.builder()
+    public String upload(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+        boolean exists = minioClient.bucketExists(BucketExistsArgs.builder().bucket(minioProperties.getBucketName()).build());
+        if (!exists) {
+            minioClient.makeBucket(MakeBucketArgs.builder()
                     .bucket(minioProperties.getBucketName())
-                    .stream(file.getInputStream(), file.getSize(), -1)
-                    .object(filename)
-                    .contentType(file.getContentType())
                     .build());
-//            String url = minioProperties.getEndpoint() + "/" + minioProperties.getBucketName() + "/" + filename;
-            String url = String.join("/",minioProperties.getEndpoint(),minioProperties.getBucketName(),filename);
-            return url;
-        }catch (Exception e){
-            e.printStackTrace();
+            minioClient.setBucketPolicy(SetBucketPolicyArgs.builder()
+                    .bucket(minioProperties.getBucketName())
+                    .config(createBucketPolicyConfig(minioProperties.getBucketName()))
+                    .build());
         }
-        return null;
+        String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+        minioClient.putObject(PutObjectArgs.builder()
+                .bucket(minioProperties.getBucketName())
+                .stream(file.getInputStream(), file.getSize(), -1)
+                .object(filename)
+                .contentType(file.getContentType())
+                .build());
+//            String url = minioProperties.getEndpoint() + "/" + minioProperties.getBucketName() + "/" + filename;
+        String url = String.join("/", minioProperties.getEndpoint(), minioProperties.getBucketName(), filename);
+        return url;
     }
+
 
     private String createBucketPolicyConfig(String bucketName) {
 
-    return """
-            {
-              "Statement" : [ {
-                "Action" : "s3:GetObject",
-                "Effect" : "Allow",
-                "Principal" : "*",
-                "Resource" : "arn:aws:s3:::%s/*"
-              } ],
-              "Version" : "2012-10-17"
-            }
-            """.formatted(bucketName);
-}
+        return """
+                {
+                  "Statement" : [ {
+                    "Action" : "s3:GetObject",
+                    "Effect" : "Allow",
+                    "Principal" : "*",
+                    "Resource" : "arn:aws:s3:::%s/*"
+                  } ],
+                  "Version" : "2012-10-17"
+                }
+                """.formatted(bucketName);
+    }
 }
