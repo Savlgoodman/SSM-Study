333e2168a93e78d0aead6f0eb27cdcf795d5f1e4
Savlgoodman <mclqw@qq.com>
2025-03-05 08:30:12 +0800

    完成了公寓信息查询接口的开发和分页查询的开发

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/ApartmentInfoServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/ApartmentInfoServiceImpl.java
@@ -1,20 +1,133 @@
 package com.atguigu.lease.web.admin.service.impl;
 
-import com.atguigu.lease.model.entity.ApartmentInfo;
+import com.atguigu.lease.model.entity.*;
+import com.atguigu.lease.model.enums.ItemType;
 import com.atguigu.lease.web.admin.mapper.ApartmentInfoMapper;
-import com.atguigu.lease.web.admin.service.ApartmentInfoService;
+import com.atguigu.lease.web.admin.service.*;
+import com.atguigu.lease.web.admin.vo.apartment.ApartmentItemVo;
+import com.atguigu.lease.web.admin.vo.apartment.ApartmentQueryVo;
+import com.atguigu.lease.web.admin.vo.apartment.ApartmentSubmitVo;
+import com.atguigu.lease.web.admin.vo.graph.GraphVo;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import kotlin.jvm.internal.Lambda;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * @author liubo
  * @description 针对表【apartment_info(公寓信息表)】的数据库操作Service实现
  * @createDate 2023-07-24 15:48:00
  */
+
+
 @Service
 public class ApartmentInfoServiceImpl extends ServiceImpl<ApartmentInfoMapper, ApartmentInfo>
         implements ApartmentInfoService {
 
+    @Autowired
+    private GraphInfoService graphInfoService;
+
+    @Autowired
+    private ApartmentFacilityService apartmentFacilityService;
+
+    @Autowired
+    private ApartmentLabelService apartmentLabelService;
+
+    @Autowired
+    private ApartmentFeeValueService apartmentFeeValueService;
+
+    @Autowired
+    private ApartmentInfoMapper apartmentInfoMapper;
+
+    @Override
+    public void saveOrUpdateApartment(ApartmentSubmitVo apartmentSubmitVo) {
+        boolean isUpdate = apartmentSubmitVo.getId() != null;
+        super.saveOrUpdate(apartmentSubmitVo);
+
+        if (isUpdate) {
+            //1.删除图片列表
+            LambdaQueryWrapper<GraphInfo> graphWrapper = new LambdaQueryWrapper<>();
+            graphWrapper.eq(GraphInfo::getItemType, ItemType.APARTMENT);
+            graphWrapper.eq(GraphInfo::getItemId, apartmentSubmitVo.getId());
+            graphInfoService.remove(graphWrapper);
+            //2.删除配套列表
+            LambdaQueryWrapper<ApartmentFacility> facilityWrapper = new LambdaQueryWrapper<>();
+            facilityWrapper.eq(ApartmentFacility::getApartmentId, apartmentSubmitVo.getId());
+            apartmentFacilityService.remove(facilityWrapper);
+            //3.删除标签列表
+            LambdaQueryWrapper<ApartmentLabel> labelWrapper = new LambdaQueryWrapper<>();
+            labelWrapper.eq(ApartmentLabel::getApartmentId, apartmentSubmitVo.getId());
+            apartmentLabelService.remove(labelWrapper);
+            //4.删除杂费列表
+            LambdaQueryWrapper<ApartmentFeeValue> feeWrapper = new LambdaQueryWrapper<>();
+            feeWrapper.eq(ApartmentFeeValue::getApartmentId, apartmentSubmitVo.getId());
+            apartmentFeeValueService.remove(feeWrapper);
+        }
+        //1.插入图片列表
+        List<GraphVo> graphVoList = apartmentSubmitVo.getGraphVoList();
+        ArrayList<GraphInfo> graphInfos = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(graphVoList)) {
+            for (GraphVo graphVo : graphVoList) {
+                GraphInfo graphInfo = new GraphInfo();
+                graphInfo.setName(graphVo.getName());
+                graphInfo.setUrl(graphVo.getUrl());
+                graphInfo.setItemId(apartmentSubmitVo.getId());
+                graphInfo.setItemType(ItemType.APARTMENT);
+                graphInfos.add(graphInfo);
+            }
+            graphInfoService.saveBatch(graphInfos);
+        }
+        //2.插入配套列表
+        List<Long> facilityInfoIds = apartmentSubmitVo.getFacilityInfoIds();
+        ArrayList<ApartmentFacility> apartmentFacilities = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(facilityInfoIds)) {
+            for (Long facilityInfoId : facilityInfoIds) {
+                ApartmentFacility apartmentFacility = new ApartmentFacility();
+                apartmentFacility.setApartmentId(apartmentSubmitVo.getId());
+                apartmentFacility.setFacilityId(facilityInfoId);
+                apartmentFacilities.add(apartmentFacility);
+            }
+            apartmentFacilityService.saveBatch(apartmentFacilities);
+        }
+        //3.插入标签列表
+        List<Long> labelIds = apartmentSubmitVo.getLabelIds();
+        if(!CollectionUtils.isEmpty(labelIds)){
+            List<ApartmentLabel> apartmentLabels = new ArrayList<>();
+            for (Long labelId : labelIds) {
+                ApartmentLabel apartmentLabel = new ApartmentLabel();
+                apartmentLabel.setApartmentId(apartmentSubmitVo.getId());
+                apartmentLabel.setLabelId(labelId);
+                apartmentLabels.add(apartmentLabel);
+            }
+            apartmentLabelService.saveBatch(apartmentLabels);
+        }
+
+        //4.插入杂费列表
+        List<Long> feeValueIds = apartmentSubmitVo.getFeeValueIds();
+        if(!CollectionUtils.isEmpty(feeValueIds)) {
+            List<ApartmentFeeValue> apartmentFeeValues = new ArrayList<>();
+            for (Long feeValueId : feeValueIds) {
+                ApartmentFeeValue apartmentFeeValue = new ApartmentFeeValue();
+                apartmentFeeValue.setApartmentId(apartmentSubmitVo.getId());
+                apartmentFeeValue.setFeeValueId(feeValueId);
+                apartmentFeeValues.add(apartmentFeeValue);
+            }
+            apartmentFeeValueService.saveBatch(apartmentFeeValues);
+        }
+    }
+
+    @Override
+    public IPage<ApartmentItemVo> pageItem(Page<ApartmentItemVo> page, ApartmentQueryVo queryVo) {
+        return apartmentInfoMapper.pageItem(page,queryVo);
+    }
 }
 
 
